e200695289c96e5a5c400b07afe6c007fd82523a
Shinji KOBAYASHI <skoba@moss.gr.jp>
2020-10-27 01:50:16 +0900

    refactored EHRbase configuration

--- config/initializers/ehrbase.rb
+++ config/initializers/ehrbase.rb
@@ -1,2 +1,44 @@
-EHRbase = YAML.load(
-  File.read("#{Rails.root}/config/ehrbase.yml"))[Rails.env]
+class EHRbase
+  class << self
+    def yaml
+      @yaml ||= YAML.load(File.read("#{Rails.root}/config/ehrbase.yml"))[Rails.env]
+    end
+
+    def yaml=(yaml)
+      @yaml = YAML.load(yaml)[Rails.env]
+    end
+
+    def scheme
+      yaml['scheme']
+    end
+
+    def host
+      yaml['host']
+    end
+
+    def port
+      yaml['port']
+    end
+
+    def path_prefix
+      yaml['path_prefix']
+    end
+
+    def url
+      yaml['url'] || "#{scheme}://#{host}:#{port}/#{path_prefix}"
+    end
+
+    def auth
+      yaml['auth']
+    end
+
+    def user
+      yaml['user']
+    end
+
+    def password
+      yaml['password']
+    end
+  end
+end
+
--- spec/rails/ehrbase_config_spec.rb
+++ spec/rails/ehrbase_config_spec.rb
@@ -1,11 +1,36 @@
 RSpec.describe 'EHRbase configuration' do
-  it 'should have scheme'
-  it 'should have host'
-  it 'should have port'
-  it 'should have path'
+  it 'should have scheme' do
+    expect(EHRbase.scheme).to eq 'http'
+  end
+
+  it 'should have host' do
+    expect(EHRbase.host).to eq 'localhost'
+  end
+
+  it 'should have port' do
+    expect(EHRbase.port).to eq 8080
+  end
+
+  it 'should have path_prefix' do
+    expect(EHRbase.path_prefix).to eq 'ehrbase/rest/openehr/v1/'
+  end
 
   it 'should have url' do
-    expect(EHRbase['url']).to eq \
+    expect(EHRbase.url).to eq \
              'http://localhost:8080/ehrbase/rest/openehr/v1/'
   end
+
+  it 'should compose url from scratch' do
+    EHRbase.yaml = EHRBASE_CONFIG_WITHOUT_URL
+    expect(EHRbase.url).to eq \
+           'http://localhost:8080/ehrbase/rest/openehr/v1/'
+  end
 end
+
+EHRBASE_CONFIG_WITHOUT_URL=<<END
+test:
+  scheme: http
+  host: localhost
+  port: 8080
+  path_prefix: ehrbase/rest/openehr/v1/
+END
