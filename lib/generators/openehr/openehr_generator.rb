1e98b1a4029fdb72715d40f1a66e2d348f0b7d96
Shinji KOBAYASHI <skoba@moss.gr.jp>
2021-05-20 11:31:32 +0900

    temporary commit

--- lib/generators/openehr/openehr_generator.rb
+++ lib/generators/openehr/openehr_generator.rb
@@ -2,45 +2,51 @@ require 'openehr'
 require 'json'
 require 'rails/generators'
 
-class ArchetypedBase < Rails::Generators::NamedBase
-  source_root File.expand_path('templates', __dir__)
-
-  def initialize(args, *options)
-    @web_template_file = args[0]
-    super
-  end
-
-  def web_template
-    @web_template ||= JSON.parse(File.read(@web_template_file))
-  end
-
-  def operational_template_file
-    @opt_file
-  end
-
-  def operational_template
-    @operational_template ||=
-      ::OpenEHR::Parser::OPTParser.new(operational_template_file).parse
-  end
-
-  def archetype_path
-    Rails.root.join('app/archetypes').to_s
-  end
-  
-  def template_name
-    operational_template.concept
-  end
-  
-  def controller_name
-    template_name.underscore
-  end
-  
-  def controller_file_path
-    controller_name
-  end
-
-  def model_name
-    
+module OpenEHR
+  module Rails
+    module Generators
+      class ArchetypedBase < Rails::Generators::NamedBase
+        source_root File.expand_path('templates', __dir__)
+        
+        def initialize(args, *options)
+          @web_template_file = args[0]
+          super
+        end
+        
+        def web_template
+          @web_template ||= JSON.parse(File.read(@web_template_file))
+        end
+        
+        def operational_template_file
+          @opt_file
+        end
+        
+        def operational_template
+          @operational_template ||=
+            ::OpenEHR::Parser::OPTParser.new(operational_template_file).parse
+        end
+        
+        def archetype_path
+          Rails.root.join('app/archetypes').to_s
+        end
+        
+        def template_name
+          operational_template.concept
+        end
+        
+        def controller_name
+          template_name.underscore
+        end
+        
+        def controller_file_path
+          controller_name
+        end
+        
+        def model_name
+          
+        end
+      end
+    end
   end
 end
 
